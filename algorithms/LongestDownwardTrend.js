8d2e1136d49b773ed10976122d6a4a08fc9e9fbf
luimu64 <leimuraittinen@gmail.com>
2021-11-18 21:35:45 +0200

    partially implemented bearish algorithm

--- algorithms/LongestDownwardTrend.js
+++ algorithms/LongestDownwardTrend.js
@@ -1,5 +1,24 @@
-const LongestDownwardTrend = () => {
-    return "dummy data";
+const LongestDownwardTrend = async (data) => {
+    let highest = 0;
+
+    let i = 0;
+    while (i < data.length) {
+        let streak = 0;
+
+        if (i === 0) {
+            i++;
+        } else {
+            while (data[i][1] < data[i - 1][1] && i < data.length) {
+                streak++;
+                i++;
+            }
+        }
+        if (streak > highest) highest = streak;
+        console.log(`streak: ${streak}`);
+        console.log(`highest: ${highest}`);
+        console.log(`i: ${i}`);
+    }
+    return highest;
 }
 
 export default LongestDownwardTrend;
