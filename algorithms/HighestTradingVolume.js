23bf4c8afe062b38ded3abb83f2360dd8ef2dfac
luimu64 <leimuraittinen@gmail.com>
2021-11-19 00:17:51 +0200

    volume comparison

--- algorithms/HighestTradingVolume.js
+++ algorithms/HighestTradingVolume.js
@@ -1,5 +1,21 @@
-const HighestTradingVolume = () => {
-    return null;
+import { DateTime } from '../lib/luxon.min.js';
+
+const compare = (a, b) => {
+    if (a.total_volumes < b.total_volumes) {
+        return 1;
+    }
+    if (a.total_volumes > b.total_volumes) {
+        return -1;
+    }
+    return 0;
+};
+
+const HighestTradingVolume = async (data) => {
+    const highestVolume = data.sort(compare);
+    console.log(highestVolume);
+    const time = DateTime.fromMillis(highestVolume[0]).toFormat('dd/MM/yyyy');
+    const volume = highestVolume[1] * data + "€";
+    return [time, volume];
 }
 
 export default HighestTradingVolume;
