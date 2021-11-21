9b1e8c0c8a51941928ff3e4cf700d1e8fb56f1ba
luimu64 <leimuraittinen@gmail.com>
2021-11-21 19:51:33 +0200

    added loading icon to display when processing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import LongestDownwardTrend from '../algorithms/LongestDownwardTrend.js';
 import HighestTradingVolume from '../algorithms/HighestTradingVolume.js';
 import BestTimeToSellAndBuy from '../algorithms/BestTimeToSellAndBuy.js';
-import { getDailyData, renderResults } from "./lib/Utility.js";
+import { getDailyData, renderResults, loading } from "./lib/Utility.js";
 
 const main = async (data, start_date, end_date) => {
+    loading.start();
     const dailyData = await getDailyData(data, start_date, end_date);
     const trend = await LongestDownwardTrend(dailyData);
     const [time, volume] = await HighestTradingVolume(dailyData);
@@ -16,6 +17,7 @@ const main = async (data, start_date, end_date) => {
         volume: volume,
         trend: trend
     });
+    loading.stop();
 }
 
 export default main;
